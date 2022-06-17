cdbd223162cad9998d979064c9508b5d28c82b87
Wagner Inácio de Oliveira <wagner.inacio16@outlook.com>
2022-06-17 17:02:29 -0300

    Modulo Fornecedor

---
+++ AppMVCBasica/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AppMVCBasica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMVCBasica.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
---
+++ AppMVCBasica/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace AppMVCBasica.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected  Entity()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
