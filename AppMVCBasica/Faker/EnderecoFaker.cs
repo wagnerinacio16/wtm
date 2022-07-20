c44c82b1b73a10a59e069296023a4e10c40aa633
Wagner Inácio de Oliveira <wagner.inacio16@outlook.com>
2022-07-20 01:04:23 -0300

    Adicionando novas funcionalides

--- AppMVCBasica/Faker/EnderecoFaker.cs
+++ AppMVCBasica/Faker/EnderecoFaker.cs
@@ -3,17 +3,21 @@ using Bogus;
 
 namespace AppMVCBasica.Faker
 {
-    public class EnderecoFaker: Faker<Endereco> 
+    public class EnderecoFaker : Faker<Endereco>
     {
-        public EnderecoFaker()
+        public Endereco dataFake()
         {
-            RuleFor(x => x.Logradouro, f => f.Address.StreetName());
-            RuleFor(x => x.Numero, f => f.Address.BuildingNumber());
-            RuleFor(x => x.Complemento, f => f.Address.SecondaryAddress());
-            RuleFor(x => x.Cep, f => f.Address.ZipCode());
-            RuleFor(x => x.Bairro, f => f.Address.Direction());
-            RuleFor(x => x.Cidade, f => f.Address.City());
-            RuleFor(x => x.Estado, f => f.Address.StateAbbr());
+            var enderecoFake = new Faker<Endereco>("pt_BR")
+           
+                .RuleFor(x => x.Logradouro, f => f.Address.StreetName())
+                .RuleFor(x => x.Numero, f => f.Address.BuildingNumber())
+                .RuleFor(x => x.Complemento, f => f.Address.SecondaryAddress())
+                .RuleFor(x => x.Cep, f => f.Address.ZipCode())
+                .RuleFor(x => x.Bairro, f => f.Address.Direction())
+                .RuleFor(x => x.Cidade, f => f.Address.City())
+                .RuleFor(x => x.Estado, f => f.Address.StateAbbr());
+          
+            return enderecoFake;
         }
     }
 }
--- AppMVCBasica/Faker/ProdutoFaker.cs
+++ AppMVCBasica/Faker/ProdutoFaker.cs
@@ -1,21 +1,25 @@
 ﻿using AppMVCBasica.Models;
 using Bogus;
+using System.Globalization;
 
 namespace AppMVCBasica.Faker
 {
-    public class ProdutoFaker: Faker<Produto>
+    public class ProdutoFaker : Faker<Produto>
     {
 
-        public ProdutoFaker()
+        public Produto dataFake()
         {
-            RuleFor(produto => produto.Nome, data => data.Commerce.ProductName());
-            RuleFor(produto => produto.Descricao, data => data.Commerce.ProductAdjective());
-            RuleFor(produto => produto.Imagem, data => data.Commerce.ProductName());
-            RuleFor(produto => produto.Valor, data => data.Random.Decimal());
-            RuleFor(produto => produto.DataCadastro, data => data.Date.Past());
-            RuleFor(produto => produto.Ativo, data => data.Random.Bool());
-            //RuleFor(produto => produto.Fornecedor, data => FornecedorFaker());
-            
+            var produtoFaker = new Faker<Produto>("pt_BR")
+
+                .RuleFor(produto => produto.Nome, data => data.Commerce.Product())
+                .RuleFor(produto => produto.Descricao, data => data.Commerce.ProductDescription())
+                //.RuleFor(produto => produto.Imagem, data => data.Commerce.ProductName())
+                .RuleFor(produto => produto.Valor, data => data.Random.Decimal(1,100000))
+                .RuleFor(produto => produto.DataCadastro, data => data.Date.Past())
+                .RuleFor(produto => produto.Ativo, data => data.Random.Bool());
+           
+            return produtoFaker;
         }
     }
 }
+
