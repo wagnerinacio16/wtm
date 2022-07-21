719c348896f37dfd5a1f64a2b2a48864544fa98c
Wagner Inácio de Oliveira <wagner.inacio16@outlook.com>
2022-07-20 22:25:37 -0300

    Adicionando projeto de console

---
+++ ConsoleAppTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using AppMVCBasica.Faker;
+using System.Text;
+namespace ConsoleAppTests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProdutoFaker produto = new ProdutoFaker();
+            EnderecoFaker endereco = new EnderecoFaker();
+            FornecedorFaker fornecedor = new FornecedorFaker();
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(gerarDadosFake(produto.dataFake()));
+
+            }
+        }
+
+        static string gerarDadosFake(object objeto)
+        {
+            var tipo = objeto.GetType();
+
+            var sb = new StringBuilder();
+
+            foreach (var p in tipo.GetProperties())
+            {
+                sb.AppendLine(p.Name + ": " + p.GetValue(objeto));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
